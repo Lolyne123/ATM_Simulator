35f0e9d8eb9f1bb71a018b8c4b120e35b0824127
OderoCeasar <156299730+OderoCeasar@users.noreply.github.com>
2025-07-16 22:47:07 +0300

    python-assignment

---
+++ simulator.py
@@ -0,0 +1,29 @@
+
+pin = 1234
+balance = 1000
+
+user_pin = int(input("Enter your PIN: "))
+
+if user_pin = pin:
+    print("Access granted")
+
+    print("1. Check Balance")
+    print("2. Withdraw Money")
+
+    choice = input("Choose an option: ")
+
+    if choice == 1:
+        print("Your balance is Ksh", balance)
+
+    elif choice == "2":
+        amount = input("Enter amount to withdraw: ")
+        if amount <= balance:
+            balance = balance - amount
+            print("Withdraw successful. New balance:", balance)
+        else
+            print("Insufficient funds")
+    else:
+        print("Invalid option")
+
+else:
+print("Access denied. Wrong PIN")
