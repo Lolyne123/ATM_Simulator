459fe3d7df04fb8ac5d43233fde0b98ad42f5259
Lolyne123 <lolyneachieng@gmail.comclear>
2025-07-17 00:24:52 +0300

    minor changes

--- simulator.py
+++ simulator.py
@@ -2,28 +2,27 @@
 pin = 1234
 balance = 1000
 
-user_pin = int(input("Enter your PIN: "))
+# prompt user to enter a pin
+user_pin = int(input("Enter your pin.")) 
 
-if user_pin = pin:
+if user_pin == pin:
     print("Access granted")
-
     print("1. Check Balance")
     print("2. Withdraw Money")
 
     choice = input("Choose an option: ")
+    if choice == '1':
+     print("Your balance is Ksh", balance)
 
-    if choice == 1:
-        print("Your balance is Ksh", balance)
-
-    elif choice == "2":
-        amount = input("Enter amount to withdraw: ")
+    elif choice == '2':
+        amount = float (input("Enter amount to withdraw: "))
         if amount <= balance:
             balance = balance - amount
             print("Withdraw successful. New balance:", balance)
-        else
+        else:
             print("Insufficient funds")
     else:
-        print("Invalid option")
+     print("Invalid option")
 
 else:
-print("Access denied. Wrong PIN")
+    print("Access denied. Wrong PIN")
